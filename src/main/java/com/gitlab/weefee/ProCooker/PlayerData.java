4424ed7e3a649c41360095f4c9bb070f4fab335c
Evie Viau <evie.viau@gmail.com>
2020-11-01 21:16:54 -0500

    Provide a hashing function for player data

--- src/main/java/com/gitlab/weefee/ProCooker/PlayerData.java
+++ src/main/java/com/gitlab/weefee/ProCooker/PlayerData.java
@@ -1,17 +1,30 @@
 package com.gitlab.weefee.ProCooker;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.util.Random;
+import java.util.Arrays;
 
 public class PlayerData {
-
     private static final String idCharacters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    private static final Path saveLocation = Path.of(System.getProperty("user.home") +
+            System.getProperty("file.separator") +
+            ".local" +
+            System.getProperty("file.separator") +
+            "share" +
+            System.getProperty("file.separator") +
+            "ProCooker");
 
     /**
      * User save data
      */
-    private static String[] saveData;
+    private static String[] saveData = new String[10];
 
     /**
      * Verifies the save data and ensure that the data is present.
@@ -25,12 +38,10 @@ public class PlayerData {
      * Creates a profile in the user's home directory
      */
     public static void createNewPlayerData() {
-        String[] saveDataPreHashed = new String[10];
-
         // Generate player ID for networking
         try {
             SecureRandom secureRandom = SecureRandom.getInstanceStrong();
-            saveDataPreHashed[0] = secureRandom.ints(20, 0, idCharacters.length())
+            saveData[1] = secureRandom.ints(20, 0, idCharacters.length())
                     .mapToObj(idCharacters::charAt)
                     .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString();
         } catch (NoSuchAlgorithmException e) {
@@ -39,15 +50,30 @@ public class PlayerData {
         }
 
         // Get the current account's username
-        saveDataPreHashed[1] = System.getProperty("user.name");
+        saveData[2] = System.getProperty("user.name");
 
         // Load all other values with 0
-        for (int i = 0; i < 8; i++) {
-            saveDataPreHashed[i+2] = "0";
+        for (int i = 0; i < 7; i++) {
+            saveData[i+3] = "0";
         }
 
         // DEBUG: Print out player ID and username
-        System.out.println(saveDataPreHashed[0] + " | " + saveDataPreHashed[1]);
+        System.out.println(saveData[1] + " | " + saveData[2]);
+
+        // Create the hash needed
+        saveData[0] = hashData(saveData);
+
+        // Create the save folder
+        try {
+            Files.createDirectories(saveLocation);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Save player data
+        if(!savePlayerData()) {
+            System.out.println("Error saving player data after creation!");
+        }
     }
 
     /**
@@ -64,5 +90,27 @@ public class PlayerData {
      */
     public static boolean loadPlayerData() {
         return false;
+    }
+
+    private static String hashData(String[] inputArray) {
+        if (inputArray.length == 10) {
+            StringBuilder hashingString = new StringBuilder();
+            for (int i = 0; i < 9; i++) {
+                hashingString.append(inputArray[i + 1]);
+            }
+            try {
+                MessageDigest digest = MessageDigest.getInstance("SHA3-256");
+                StringBuilder hash = new StringBuilder();
+                byte[] hashedBytes = digest.digest(
+                        hashingString.toString().getBytes(StandardCharsets.UTF_8));
+                for (byte b: hashedBytes) {
+                    hash.append(String.format("%02X", b));
+                }
+                return hash.toString();
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+        }
+        return "NULL";
     }
 }
