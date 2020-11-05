2952c4ad4f8dc9b6e6649602542649784f5ebe18
aphawvn <aphawvn@gmail.com>
2020-11-05 02:40:19 -0500

    started work on Controller

---
+++ src/main/java/com/gitlab/weefee/ProCooker/Controller.java
@@ -0,0 +1,29 @@
+package com.gitlab.weefee.ProCooker;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Controller implements ActionListener {
+	
+	private CookingGame game;
+	
+	private JButton[] buttons;
+	private JButton reset;
+	private JButton finishCooking;
+	
+	public Controller(CookingGame game, JButton[] buttons, JButton reset, JButton finishCooking) {
+		this.game = game;
+		this.buttons = buttons;
+		this.reset = reset;
+		this.finishCooking = finishCooking;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		
+		try {
+			
+		}
+		
+	}
+
+}
